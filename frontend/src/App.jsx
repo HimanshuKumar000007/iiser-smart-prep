6d4b01e824e370357caa24887eb7c28e60506df3
HimanshuKumar0007 <140370652+HimanshuKumar0007@users.noreply.github.com>
2026-03-20 11:54:05 +0530

    Add biological diversity mindmap and React entry points, remove obsolete JSX file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import BiologyMindMap from "./subjects/short-notes/biology/iat-biology-mindmap";
+
+export default function App() {
+    return (
+        <div>
+            <BiologyMindMap />
+        </div>
+    );
+}
